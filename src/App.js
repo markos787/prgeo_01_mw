0ae7d98fd0a657dc6c0dec45031ef663cc6d1921
markos787 <149382126+markos787@users.noreply.github.com>
2024-05-19 21:46:43 +0200

    wip: map + database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 import Home from "./components/home/Home";
+import Services from "./components/services/Services";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Map from "./components/map/Map";
+// import { Dashboard } from "@mui/icons-material";
+import Dashboard from "./components/dashboard/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/services/map",
+    element: <Map />,
+  },
+  {
+    path: "/services/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
   const imie = "tomasz";
 
   return (
     <div className="App">
-      <Home text={imie} />
+      <RouterProvider router={router} />
     </div>
   );
 }
